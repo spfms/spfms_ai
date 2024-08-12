8bc28a5bceebac234ce9ce738a240b1cfc64f4bf
Ali Ebrahimi <a.ebrahimi@mohaymen.ir>
2024-08-12 20:59:37 +0330

    Implement stock news crawl

--- crawling/stock_news_crawl.py
+++ crawling/stock_news_crawl.py
@@ -3,11 +3,33 @@ import time
 
 import requests
 from bs4 import BeautifulSoup
+from persiantools.jdatetime import JalaliDateTime, JalaliDate
 
-base_url = ("https://www.tasnimnews.com/fa/service/84/%D8%A8%D8%A7%D8%B2%D8%A7%D8%B1-%D8%B3%D9%87%D8%A7%D9%85-%D8%A8"
+base_url = ("https://www.tasnimnews.com/fa/service/84/%D8%A8%D8%A7%D8%B2%D8%A7%D8%B1-%D8%B3%D9%87%D8%A7%D9%85-%D8%B3"
             "%D9%88%D8%B1%D8%B3?page=")
 
-output_file = 'stock_news.csv'
+output_file = '../datasets/stocknews/stock_news.csv'
+
+
+def replace_arabic_characters(text):
+    text = text.replace('ي', 'ی')
+    text = text.replace('ك', 'ک')
+
+    return text
+
+
+def convert_date(date_str):
+    date_str = replace_arabic_characters(date_str)
+
+    if "پیش" in date_str:
+        return JalaliDate.today().strftime('%Y/%m/%d')
+
+    try:
+        date_part, time_part = date_str.split(' - ')
+        jalali_date = JalaliDateTime.strptime(date_part, '%d %B %Y', 'fa').jalali_date()
+        return jalali_date.strftime('%Y/%m/%d')
+    except ValueError:
+        return date_str
 
 
 def scrape_page(page_number):
@@ -22,6 +44,7 @@ def scrape_page(page_number):
         for article in news_section.find_all('article', class_='list-item'):
             time_tag = article.find('time')
             date = time_tag.text.strip() if time_tag else 'N/A'
+            date = convert_date(date)
 
             title_tag = article.find('h2', class_='title')
             title = title_tag.text.strip() if title_tag else 'N/A'
@@ -41,9 +64,9 @@ def scrape_all_pages(start_page, end_page):
             news_items = scrape_page(page)
             writer.writerows(news_items)
 
-            time.sleep(0.1)
+            time.sleep(0.001)
 
 
-scrape_all_pages(1, 700)
+scrape_all_pages(1, 746)
 
 print(f"Scraping completed. Data saved to {output_file}")
