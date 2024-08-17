4d94f14226f80126736b561622dc9704e3be3e1d
Ali Ebrahimi <a.ebrahimi@mohaymen.ir>
2024-08-17 20:13:23 +0330

    Add stock market predictions api

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
+import json
+import os
+
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
+@app.route('/stock-market-predictions', methods=['GET'])
+def get_predictions():
+    json_path = os.path.join('datasets', 'predictions', 'stock_market_predictions.json')
+
+    try:
+        with open(json_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return jsonify({"error": "File not found"}), 404
+    except json.JSONDecodeError:
+        return jsonify({"error": "Error decoding JSON"}), 500
+
+    return jsonify(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
