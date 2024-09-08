4d7479a90bd054ea9defe2c25eb848494ddd86aa
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2024-09-08 18:06:40 +0330

    update cleaned adjusted_price

--- processing/adjusted_price_cleaner.py
+++ processing/adjusted_price_cleaner.py
@@ -6,18 +6,26 @@ from utils.alphabet_util import clean_sentence
 file_path = '../datasets/stockprices/Adjusted_Price_report.csv'
 output_path = '../datasets/stockprices/Cleaned_Adjusted_Price_report.csv'
 
+# Read the CSV file
 df = pd.read_csv(file_path)
 
+# Apply cleaning to 'Ticker' column
 df['Ticker'] = df['Ticker'].apply(clean_sentence)
 
 
+# Define the function to reformat Persian dates to Gregorian dates
 def reformat_date(persian_date):
     jalali_date = jdatetime.date(*map(int, persian_date.split('-')))
     return jalali_date.strftime('%Y/%m/%d')
 
 
+# Apply date reformatting
 df['Date'] = df['Date'].apply(reformat_date)
 
+tickers_to_remove = ['وکغدیر1', 'اردستان1', 'کیمیا1', 'ومهان1']
+df = df[~df['Ticker'].isin(tickers_to_remove)]
+
+# Save the cleaned DataFrame to a new CSV file
 df.to_csv(output_path, index=False)
 
 print("File updated successfully!")
