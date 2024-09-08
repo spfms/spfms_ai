4469d907459281aaf1523589a3eb57b6c8102f1f
Ali Ebrahimi <aliebrahimi6739@gmail.com>
2024-09-08 20:21:27 +0330

    update api for mng portfolio

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import json
 import os
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+
+from portfolio.portfolio_management import manage_portfolio, get_all_tickers
 
 app = Flask(__name__)
 
@@ -24,6 +26,30 @@ def get_predictions():
         return jsonify({"error": "Error decoding JSON"}), 500
 
     return jsonify(data), 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+
+@app.route('/tickers', methods=['GET'])
+def get_stock_tickers():
+    try:
+        tickers = get_all_tickers()
+        return jsonify({'stock-tickers': tickers}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/manage-stock-portfolio', methods=['POST'])
+def manage_stock_portfolio():
+    try:
+        invested_amounts_dict = request.json.get('invested_amounts_dict')
+
+        if not invested_amounts_dict:
+            return jsonify({'error': 'Invalid input: invested_amounts_dict is required'}), 400
+
+        portfolio_data = manage_portfolio(invested_amounts_dict)
+
+        return jsonify(portfolio_data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 if __name__ == '__main__':
